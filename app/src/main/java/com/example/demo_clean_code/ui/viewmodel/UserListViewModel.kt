06a6df043ae16d4d330ea3eacddf40e393e6e4fd
Shahriaz <montakimshuvo@gmail.com>
2024-10-30 14:38:02 +0600

    integrate fetchUsers function in view model

--- app/src/main/java/com/example/demo_clean_code/ui/viewmodel/UserListViewModel.kt
+++ app/src/main/java/com/example/demo_clean_code/ui/viewmodel/UserListViewModel.kt
@@ -1,7 +1,29 @@
 package com.example.demo_clean_code.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.demo_clean_code.data.model.UserEntity
+import com.example.demo_clean_code.domain.AddUserUseCase
+import com.example.demo_clean_code.domain.ClearUsersUseCase
+import com.example.demo_clean_code.domain.DeleteUserUseCase
+import com.example.demo_clean_code.domain.GetUsersUseCase
+import com.example.demo_clean_code.domain.NetworkStateResources
+import com.example.demo_clean_code.domain.SearchUsersUseCase
+import com.example.demo_clean_code.ui.model.SnackbarEffect
+import com.example.demo_clean_code.ui.model.UserIntent
+import com.example.demo_clean_code.ui.model.UserViewState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,10 +33,55 @@ class UserListViewModel @Inject constructor(
     private val deleteUserUseCase: DeleteUserUseCase,
     private val clearUsersUseCase: ClearUsersUseCase,
     private val searchUsersUseCase: SearchUsersUseCase,
-    private val saveThemeUseCase: SaveThemeUseCase,
-    private val getThemeUseCase: GetThemeUseCase
 ):ViewModel() {
 
-    private val _
+    private val _viewState = MutableStateFlow(UserViewState())
+    val viewState : StateFlow<UserViewState> = _viewState
+
+    private val _effectChannel = Channel<SnackbarEffect>()
+    val effectFlow : Flow<SnackbarEffect> = _effectChannel.receiveAsFlow()
+
+    private var recentlyDeletedUser: UserEntity? = null
+
+    private var searchJob : Job? = null
+
+
+    private fun loadUsers(){
+        viewModelScope.launch(Dispatchers.IO) {
+            getUsersUseCase().collectLatest {
+                resource->
+                when(resource){
+                    is NetworkStateResources.Loading -> withContext(Dispatchers.Main){
+                        _viewState.update {
+                            it.copy(isLoading = true)
+                        }
+                    }
+
+                    is NetworkStateResources.Success -> withContext(Dispatchers.Main){
+                        _viewState.update {
+                            it.copy(
+                                isLoading = false,
+                                users = resource.data,
+                                filteredUserList = resource.data,
+
+                            )
+                        }
+                    }
+
+                    is NetworkStateResources.Error -> withContext(Dispatchers.Main){
+                        _viewState.update {
+                            it.copy(
+                                isLoading = false
+                            )
+                        }
+                        _effectChannel.send(SnackbarEffect.ShowSnackbar("Error loading users: ${resource.message}"))
+                    }
+                }
+            }
+        }
+    }
+
+
+
 
 }
